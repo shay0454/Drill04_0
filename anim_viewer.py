dc16ee4ac661263086d289827ec45ec81534e9b6
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-09-23 14:52:15 +0900

    find size of cilp

--- anim_viewer.py
+++ anim_viewer.py
@@ -1,5 +1,13 @@
 from pico2d import *
 open_canvas()
 os.chdir('c:\\github\\2Dprogramming\\Drill04')
-
+character=load_image('moving_character.png')
+x,y=400,300
+frame=0
+while(True):
+    clear_canvas()
+    character.clip_draw(frame*137+1,0,135,135,x,y)
+    frame=(frame+1)%6
+    update_canvas()
+    get_events()
 close_canvas()
