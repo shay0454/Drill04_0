6df6b91c4808dba57a6e237bacbf8383dedd288e
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-09-23 16:42:21 +0900

    remove orgin, rename variable, add comment, remove code for check

--- anim_viewer.py
+++ anim_viewer.py
@@ -1,22 +1,18 @@
 from pico2d import *
 open_canvas()
 os.chdir('c:\\github\\2Dprogramming\\Drill04')
-ch1=load_image('moving_character.png')
-ch1_size=[137,137]
-ch1_frame,ch1_per=24,[6,6,6,6]
 ch=load_image('character_v2.png')
-ch_size=[32,32]
-ch_frame,ch_per=[13,8],[7,4,6,10,10,10,8,13]
-x,y=400,300
-frame,frame_per=0,0
+ch_size=[32,32] #cilp 크기
+ch_frame,ch_nums=[13,8],[7,4,6,10,10,10,8,13] # 시트에서 클립의 최대 가로,세로 개수, 각 세로의 개수
+x,y=400,300 #위치
+frame,ch_nums_index=0,0 # 본 프레임(하나의 타이머), ch_nums의 값을 쓰기 위한 index 저장 변수
 while(True):
     clear_canvas()
-    print(frame, ch_per[frame_per],frame_per)
     ch.clip_draw((frame%ch_frame[0])*ch_size[0],(frame//ch_frame[0])*ch_size[1],ch_size[0],ch_size[1],x,y)
     frame=(frame+1)%(ch_frame[0]*ch_frame[1])
-    if((frame)%ch_frame[0]==ch_per[frame_per]%ch_frame[0]):
-        frame+=ch_frame[0]-ch_per[frame_per]
-        frame_per=(frame_per+1)%len(ch_per)
+    if((frame)%ch_frame[0]==ch_nums[ch_nums_index]%ch_frame[0]):
+        frame+=ch_frame[0]-ch_nums[ch_nums_index]
+        ch_nums_index=(ch_nums_index+1)%len(ch_nums)
     update_canvas()
     delay(0.05)
     get_events()
